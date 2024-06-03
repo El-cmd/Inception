7d53b25b48f3955385c1959976f0d2cd0bf35e8a
Valentin LOTH <vloth@paul-f4Ar4s4.clusters.42paris.fr>
2024-06-04 01:52:49 +0200

    debug mariadb

--- srcs/requirements/mariadb/tableSQL.sh
+++ srcs/requirements/mariadb/tableSQL.sh
@@ -3,14 +3,17 @@
 #demarrer MySQL
 service mysql start;
 
+# Attendre que MariaDB soit complètement démarré
+sleep 10
+
 #Creation de la table si elle n'existe pas deja avec le nom stocker dans le .env
-mysql -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 
 #Creation d'un utilisateur avec un mdp qui pourra manipuler la table
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 #Donner les droits de la table à lutilisateur qui vient detre créer
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'localhost';"
 
 #Modifacation de lutilisateur root pour lui donner un mdp -> + de securité
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
