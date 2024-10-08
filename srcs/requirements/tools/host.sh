9cfa1d972f03c652fae45fff11a4f71417a13de1
El-cmd <58914622+El-cmd@users.noreply.github.com>
2024-10-08 20:00:08 +0200

    Delete srcs directory

--- srcs/requirements/tools/host.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Renomer la machine sur le reseau
-
-sudo hostnamectl set_hostname vloth.42.fr
-echo "Penser à modifier le fichier /etc/hosts et de mettre le nouveau nom de la machine"
